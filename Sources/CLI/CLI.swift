fe7d0beffa68e9f02d54e1da5bff5c3faf6b16a0
Juri Pakaste <juri@juripakaste.fi>
2024-05-12 17:06:24 +0300

    Add CLI tool dotenvy-tool and subcommand check
    check checks the syntax of the input file or stdin.

---
+++ Sources/CLI/CLI.swift
@@ -0,0 +1,69 @@
+import ArgumentParser
+import DotEnvy
+import Foundation
+
+@main
+struct Tool: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "dotenvy-tool",
+        abstract: "Tool for working with dotenv files",
+        subcommands: [Check.self]
+    )
+}
+
+struct Check: ParsableCommand {
+    static var configuration
+        = CommandConfiguration(
+            abstract: "Check syntax of input.",
+            discussion: """
+            In case of a syntax error, the error is printed out and the command exits
+            with failure code \(ExitCode.failure.rawValue).
+
+            If there are no problems reading the input, nothing is printed and the command exits
+            with \(ExitCode.success.rawValue).
+            """
+        )
+
+    @Argument(help: "Input file. Standard input is used if omitted")
+    var file: FileURL?
+
+    func validate() throws {
+        _ = try self.file?.url.checkResourceIsReachable()
+    }
+
+    func run() throws {
+        let string = try readInput(self.file)
+        do {
+            _ = try DotEnvironment.parse(string: string)
+        } catch let error as ParseErrorWithLocation {
+            print(error.formatError(source: string))
+            throw ExitCode.failure
+        }
+    }
+}
+
+struct FileURL: ExpressibleByArgument {
+    var url: URL
+
+    init?(argument: String) {
+        // the new URL(filePath:directoryHint:) is not available on Linux
+        let url = URL(fileURLWithPath: argument, isDirectory: false)
+        guard url.isFileURL else {
+            return nil
+        }
+        self.url = url
+    }
+}
+
+private func readInput(_ file: FileURL?) throws -> String {
+    let data: Data
+    if let file {
+        data = try Data(contentsOf: file.url)
+    } else {
+        data = FileHandle.standardInput.readDataToEndOfFile()
+    }
+    guard let string = String(data: data, encoding: .utf8) else {
+        throw ValidationError("Input could not be decoded as UTF-8")
+    }
+    return string
+}
